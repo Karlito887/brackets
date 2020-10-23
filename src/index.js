85151023f870a28038c89ba011a107ac9d50cdb2
Nazarii Karlyk <karliknazar@gmail.com>
2020-10-23 13:53:10 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = Object.fromEntries(bracketsConfig)
+    let stack = []
+
+    for (let item of str) {
+        let isSame = item === brackets[item]
+        if(isSame) {
+            if(stack.includes(brackets[item] + 'c')) {
+                if (stack.pop() !== item + 'c') {
+                    return false;
+                }
+            } else stack.push(brackets[item] + 'c')
+        } else if (brackets[item]) {
+            stack.push(brackets[item])
+        } else if (stack.pop() !== item) {
+            return false;
+        }
+    }
+    return stack.length === 0
 }
